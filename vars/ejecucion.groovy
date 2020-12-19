737c1b000d1bd806aa6bc027a72501f3c2cf09c9
Alexander Sanhueza <alexander.ksc@gmail.com>
2020-12-19 15:10:43 -0300

    probando separacion varaiables v1.6

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -4,10 +4,16 @@ def call(){
         agent any
 
         parameters { 
-            choice(name: 'TECNOLOGIA', choices: ['maven', 'gradle'], description: 'Elección de herramienta de empaquetamiento')
+            choice(name: 'TECNOLOGIA', choices: ['gradle', 'maven'], description: 'Elección Herramienta de automatización de construcción de código de software')
 
-            string(name: 'STAGE', defaultValue: '', description: 'Elección de cual etapa Pipeline')
+            string(name: 'STAGE', defaultValue: '', description: 'Elección de cual etapa elegir a procesar dentro del Pipeline')
         }
+
+        //lo que debe hacer
+        //1. Parametro vacio = todos los pasos
+        //2. Parametro 1 o N = Puede ser 1 o Varios pasos separados por ";"
+        //3. Validar que el STAGE ingresado ësta disponible dentro del pipeline y la herramienta a utilizar gradle o maven
+        //4. Dar aviso si se ingreso un paso no existe
 
 
         stages {
@@ -15,18 +21,48 @@ def call(){
                 steps{
                     script{
                             try {
-                                println('Hola Mundo')
-                                println(params.TECNOLOGIA + params.STAGE)
-                                echo "Hello ${params.TECNOLOGIA}"
-                                echo "Hello ${params.STAGE}"
+                                String tec = params.TECNOLOGIA.toUpperCase()
+                                String stage = params.STAGE.toUpperCase()
+                                String[] etapas;
+                                //Defino Arreglo de Pasos Existentes por Tecnologia
+                                def gradle_pasos = ['BUILD', 'TEST', 'SONAR', 'INICIAR','TEST_REST','NEXUS']; 
+                                def maven_pasos = ['BUILD', 'TEST','JAR_CODE', 'SONAR', 'INICIAR','TEST_REST'];
+                                echo "TEC ingreso: ${params.TECNOLOGIA}" 
+                                echo "TEC despues: ${tec}" 
+                                //Reviso si los pasos ingresados corresponden a los existentes, si no envio error
+                                if (stage.le)
+                                resultado = stage.length()>0 ? true : false
+                                //Comprobe que se ingresaron etapas
+                                if(resultado){
+                                    etapas = stage.split(';');
+
+                                    switch(tec) {
+                                        case 'gradle':
+                                            for( String _et : etapas )  
+                                                gradle_pasos.contains(_et) ? println('etapas : ' + _et) : error ('La etapa : ' + _et + ' no es valida, favor ingrese una existente, dentro de los valores son : BUILD\nTEST\nSONAR\nINICIAR\nTEST?REST\nNEXUS ')
+                                            }
+                                        break
+                                        case 'maven':
+                                          
+                                        break
+
+                                    }
+      
+                                }
+                              
+                                //println('Hola Mundo') // y
+                                //println(params.TECNOLOGIA + params.STAGE) // y
+                                //echo "Hello ${params.TECNOLOGIA}" // y
+                                //echo "Hello ${params.STAGE}" // y
                             } catch(Exception e) {
-                                println('ERRRRRROOOOOR')
-                                error ('Ha ocurrido el siguiente error: ' + e)
+                                error ('Ha ocurrido un error inesperado en ValidacionPametros: ' + e)
                             }
                         }
                     }//fin steps validacionParametros
             }// fin stage validacionParametros
             
+
+
             /*
             stage('Pipeline') {
                 //Estructura de Stages Maven y Gradle
