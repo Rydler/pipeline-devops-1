6051b6c38714c761e84a5ad723db2cb0fbd187a8
Alexander Sanhueza <alexander.ksc@gmail.com>
2020-12-19 17:17:58 -0300

    probando etapas v1.0

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -39,11 +39,59 @@ def todos_los_pasos(){
 }
 
 def build(){
-    stage('BUILD'){
+    stage('Build'){
         env.TAREA = 'build'
-        //sh 'gradle clean build' //gradlew es para que se ejecute con wrapper
-        sh 'echo hola mundo'
-    } //end build
+        //sh 'gradle clean build' 
+        sh 'echo build'
+    } //end 
+}
+
+def test(){
+    stage('Test'){
+        env.TAREA = 'test'
+        //sh 'gradle clean test' 
+         sh 'echo test'
+    } //end 
+}
+
+def sonar(){
+    stage('Sonar'){
+        env.TAREA = 'sonar'
+        /*
+        //SonnarScanner
+        def scannerHome = tool 'sonar-scanner';
+        //Sonnar Server
+        withSonarQubeEnv('sonar'){
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+        }
+        */
+        sh 'echo sonar'
+    } //end 
+}
+def iniciar(){
+    stage('Run'){
+        env.TAREA = 'run'
+        //sh 'nohup gradle bootRun &'
+        //sleep 20
+        sh 'echo run'
+    } //end 
+}
+
+def test_rest(){
+    stage('Rest'){
+        env.TAREA = 'rest'
+        //sleep 20
+        //sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+        sh 'echo rest'
+    } //end 
+}
+
+def nexus(){
+    stage('Nexus'){
+        env.TAREA = 'nexus'
+        //nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/kuroi/.jenkins/workspace/_multibranch_feature-dir-inicial/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+        sh 'echo nexus'
+    } //end 
 }
 
 return this;
