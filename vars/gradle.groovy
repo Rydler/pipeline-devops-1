9132ed935d8d442bb57458916874314f9b286838
Alexander Sanhueza <alexander.ksc@gmail.com>
2021-01-05 02:05:46 -0300

    Taller 15 v2.5

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -9,42 +9,17 @@ def call(String etapasEscogidas){
     def funciones   = new Funciones()
     def etapas      = funciones.validarEtapasValidas(etapasEscogidas, gradle_pasos)
 
-    println(etapas)
-   
-}
-
-def todos_los_pasos(){
-  
-    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
-    stage('build & test'){
-         env.TAREA = 'build & test'
-        sh 'gradle clean build' //gradlew es para que se ejecute con wrapper
-    } //end build
-    stage('sonar'){
-         env.TAREA = 'sonar'
-        //SonnarScanner
-        def scannerHome = tool 'sonar-scanner';
-        //Sonnar Server
-        withSonarQubeEnv('sonar'){
-            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+    etapas.each{
+        stage(it){
+            try{
+                //Llamado dinamico
+                println(${it.toLowerCase()})
+            }catch(Exception e) {
+                error "Stage ${it} tiene problemas : ${e}"
+            }
         }
-        
-    }//end test
-    stage('run'){
-        env.TAREA = 'run'
-        sh 'nohup gradle bootRun &'
-        sleep 20
-    }//end run
-    stage('rest'){
-        env.TAREA = 'rest'
-        sleep 20
-        sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-    }//end run
-    stage('nexus'){
-        env.TAREA = 'nexus'
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/kuroi/.jenkins/workspace/_multibranch_feature-dir-inicial/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-    }//end nexus
-
+    }
+   
 }
 
 def build(){
