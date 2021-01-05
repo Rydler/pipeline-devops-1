191c2c4ff6f673080bb046306e61f41157663b3b
Alexander Sanhueza <alexander.ksc@gmail.com>
2021-01-05 14:40:03 -0300

    Taller 15 CI Gradle v1.5

--- vars/ci_gradle.groovy
+++ vars/ci_gradle.groovy
@@ -2,14 +2,14 @@ import pipeline.utilidades.*
 
 def call(String etapasEscogidas){
 
-    //Defino Arreglo de Pasos Existentes por Tecnologia en CI
-    def ci_gradle_pasos = ['BUILDANDTEST'];
+    //Defino Arreglo de Pasos Existentes por Tecnologia
+    def gradle_pasos = ['BUILD', 'TEST', 'SONAR', 'INICIAR','TEST_REST','NEXUS'];
 
     env.Tarea = 'Gradle CI Pipeline'
     figlet env.Tarea
 
     def funciones   = new Funciones()
-    def etapas      = funciones.validarEtapasValidas(etapasEscogidas, ci_gradle_pasos)
+    def etapas      = funciones.validarEtapasValidas(etapasEscogidas, gradle_pasos)
 
     etapas.each{
         stage(it){
@@ -25,12 +25,67 @@ def call(String etapasEscogidas){
    
 }
 
-def buildAndTest(){
+
+def build(){
     script{
-        env.Tarea = 'buildAndTest'
+        env.Tarea = 'build'
         figlet env.Tarea
     }
-    sh 'gradle clean build'
+    sh 'gradle clean build' 
+    //sh 'echo build'
+}
+
+def test(){
+    script{
+        env.Tarea = 'test'
+        figlet env.Tarea
+    }
+    sh 'gradle test' 
+    //sh 'echo test'
+}
+
+def sonar(){
+    script{
+        env.Tarea = 'sonar'
+        figlet env.Tarea
+    }
+        
+    //SonnarScanner
+    def scannerHome = tool 'sonar-scanner';
+    //Sonnar Server
+    withSonarQubeEnv('sonar'){
+        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+    }
+        
+    //sh 'echo sonar'
+}
+def iniciar(){
+    script{
+        env.Tarea = 'run'
+        figlet env.Tarea
+    }
+    sh 'nohup gradle bootRun &'
+    sleep 20
+    //sh 'echo run'
+}
+
+def test_rest(){
+    script{
+        env.Tarea = 'rest'
+        figlet env.Tarea
+    }
+    sleep 20
+    sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+    //sh 'echo rest'
+}
+
+def nexus(){
+    script{
+        env.Tarea = 'nexus'
+        figlet env.Tarea
+    }
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/kuroi/.jenkins/workspace/_multibranch_feature-dir-inicial/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    //sh 'echo nexus'
 }
 
 return this;
