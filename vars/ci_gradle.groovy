e73690d9fcc65067d21554ad31c6f117a7f46511
Alexander Sanhueza <alexander.ksc@gmail.com>
2021-01-05 16:56:04 -0300

    Taller 15 CI Gradle v1.27

--- vars/ci_gradle.groovy
+++ vars/ci_gradle.groovy
@@ -84,7 +84,7 @@ def nexus(){
 
         println("${WORKSPACE}/build/libs/${env.ProyectoArtefactoID}-${env.ProyectoVersion}.jar")
     }
-    //nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'ci-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "${WORKSPACE}"'/build/libs/'${env.ProyectoArtefactoID}'-'${env.ProyectoVersion}'.jar']], mavenCoordinate: [artifactId: env.ProyectoArtefactoID, groupId: env.ProyectoGrupoID, packaging: 'jar', version: ${env.ProyectoVersion}]]]
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'ci-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "${WORKSPACE}/build/libs/${env.ProyectoArtefactoID}-${env.ProyectoVersion}.jar"]], mavenCoordinate: [artifactId: env.ProyectoArtefactoID, groupId: env.ProyectoGrupoID, packaging: 'jar', version: ${env.ProyectoVersion}]]]
     //nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "${WORKSPACE}"'/build/libs/'"${env.ProyectoArtefactoID}"'-'"${env.ProyectoVersion}"'.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
     //sh 'echo nexus'
 }
